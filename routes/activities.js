dc3756cb86c14b40e1e4ea23f0961f5f8dce4478
Luis Lopez <luis.lopez3664mail.com>
2023-09-07 17:32:27 -0300

    agregando passaport con los otros router

--- routes/activities.js
+++ routes/activities.js
@@ -2,11 +2,12 @@ import express from 'express';
 const routerActivities = express.Router();
 
 import activitiesControllers from '../controllers/activitiesControllers.js';
+import passport from '../middlewares/passport.js';
 
-routerActivities.post('/', activitiesControllers.createActivity);
+routerActivities.post('/', passport.authenticate( 'jwt', {session:false} ), activitiesControllers.createActivity);
 routerActivities.get('/', activitiesControllers.getAllActivities);
 routerActivities.get('/:id', activitiesControllers.getActivityById);
-routerActivities.put('/:id', activitiesControllers.updateActivityById);
-routerActivities.delete('/:id', activitiesControllers.deleteActivityById);
+routerActivities.put('/:id', passport.authenticate( 'jwt', {session:false} ), activitiesControllers.updateActivityById);
+routerActivities.delete('/:id', passport.authenticate( 'jwt', {session:false} ), activitiesControllers.deleteActivityById);
 
 export default routerActivities;
--- routes/itineraries.js
+++ routes/itineraries.js
@@ -2,11 +2,12 @@ import express from 'express';
 const routerItineraries = express.Router();
 
 import itinerariesControllers from '../controllers/itinerariesControllers.js';
+import passport from '../middlewares/passport.js';
 
-routerItineraries.post('/', itinerariesControllers.createItinerary);
+routerItineraries.post('/', passport.authenticate( 'jwt', {session:false} ), itinerariesControllers.createItinerary);
 routerItineraries.get('/', itinerariesControllers.getAllItineraries);
 routerItineraries.get('/:id', itinerariesControllers.getItineraryById);
-routerItineraries.put('/:id', itinerariesControllers.updateItineraryById);
-routerItineraries.delete('/:id', itinerariesControllers.deleteItineraryById);
+routerItineraries.put('/:id', passport.authenticate( 'jwt', {session:false} ), itinerariesControllers.updateItineraryById);
+routerItineraries.delete('/:id', passport.authenticate( 'jwt', {session:false} ), itinerariesControllers.deleteItineraryById);
 
 export default routerItineraries;
